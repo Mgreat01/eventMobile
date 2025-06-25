846acc299d100d37e01a778b4528f3deb0423d19
ephraimmonga5 <ephraimmonga5@gmail.com>
2025-06-25 20:39:35 +0000

    implementation du model loginReponse

--- lib/business/models/user/loginReponse.dart
+++ lib/business/models/user/loginReponse.dart
@@ -8,11 +8,21 @@ class LoginResponse {
     required this.message,
     required this.user,
   });
-
+/*
   factory LoginResponse.fromJson(Map<String, dynamic> json) {
     final userData = json['data'] ?? {};
     return LoginResponse(
       message: json['message'] ?? '',
+      user: User.fromJson(userData),
+    );
+  }
+*/
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) {
+    final Map<String, dynamic> map = Map<String, dynamic>.from(json);
+    final userData = Map<String, dynamic>.from(map['data'] ?? {});
+    return LoginResponse(
+      message: map['message'] ?? '',
       user: User.fromJson(userData),
     );
   }
