b1f4a458492a1f02cfe893e5ed7cef33e7b959cf
ephraimmonga5 <ephraimmonga5@gmail.com>
2025-06-25 20:47:20 +0000

    ajout des nouvelles methodes dans la classe abstraite

--- lib/business/services/user/userNetworkService.dart
+++ lib/business/services/user/userNetworkService.dart
@@ -1,11 +1,12 @@
 import '../../models/user/authentication.dart';
+import '../../models/user/interet.dart';
 import '../../models/user/user.dart';
 
 abstract class UserNetworkService {
   Future<User> seConnecter(Authentication authentication);
   Future<User> recupererInfoUtilisateur();
-  Future<User> seDeconnecter();
   Future<User> creerCompte(User user);
-
-
+  Future<User> seDeconnecter();
+  Future<List<Interet>> getInterets();
+  Future<bool> verifierOtp({required String email, required String otp});
 }
