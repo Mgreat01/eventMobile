206a2c69309b5376f8710bf35481e84c74267fed
ephraimmonga5 <ephraimmonga5@gmail.com>
2025-06-25 20:57:40 +0000

    implementation de la page otpState

--- lib/pages/otp/otpState.dart
+++ lib/pages/otp/otpState.dart
@@ -1,1 +1,29 @@
-it 
+// otpVerificationState.dart
+
+class OtpVerificationState {
+  final bool isLoading;
+  final bool isVerified;
+  final String? error;
+  final String? email;
+
+  OtpVerificationState({
+    this.isLoading = false,
+    this.isVerified = false,
+    this.error,
+    this.email,
+  });
+
+  OtpVerificationState copyWith({
+    bool? isLoading,
+    bool? isVerified,
+    String? error,
+    String? email,
+  }) {
+    return OtpVerificationState(
+      isLoading: isLoading ?? this.isLoading,
+      isVerified: isVerified ?? this.isVerified,
+      error: error,
+      email: email ?? this.email,
+    );
+  }
+}
